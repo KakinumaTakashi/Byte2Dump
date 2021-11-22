9f6e92597c8d340c3c41ba0efd2bdff96f0c1969
Kakinuma Takashi <kakinuma.takashi@homes.ecweb.jp>
2021-11-22 17:11:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Byte2Dump/ByteToDump.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class ByteToDump
+    {
+        /// <summary>
+        /// <para>ヘッダ行を出力する間隔(行数)</para>
+        /// <para>0以下の数値を指定した場合はヘッダは出力されません。</para>
+        /// </summary>
+        public int HeaderSpan { get; set; } = 16;
+        /// <summary>
+        /// <para>ヘッダ行をダンプの１行目のみに出力する。</para>
+        /// <para>true - 有効, false - 無効</para>
+        /// </summary>
+        public bool HeaderFirstLineOnly { get; set; } = true;
+
+        /// <summary>
+        /// 内部バッファ
+        /// </summary>
+        public byte[] ByteData = new byte[0];
+
+
+        public ByteToDump()
+        {
+        }
+
+        public ByteToDump(byte[] byteData)
+        {
+            this.SetByte(byteData);
+        }
+
+        /// <summary>
+        /// バイトデータをセットする。
+        /// </summary>
+        /// <param name="byteData"></param>
+        public void SetByte(byte[] byteData)
+        {
+            this.ByteData = byteData;
+        }
+
+        /// <summary>
+        /// バイトデータを末尾に追加する。
+        /// </summary>
+        /// <param name="byteData"></param>
+        public void Add(byte[] byteData)
+        {
+            int _destOffset = this.ByteData.Length;
+            Array.Resize(ref this.ByteData, this.ByteData.Length + byteData.Length);
+            Array.Copy(byteData, 0, this.ByteData, _destOffset, byteData.Length);
+        }
+
+        /// <summary>
+        /// ヘッダ定義
+        /// </summary>
+        private const string DUMP_HEADER = "\n" +
+            "ADDRESS  +0 +1 +2 +3 +4 +5 +6 +7  +8 +9 +A +B +C +D +E +F ASCII\n" +
+            "--------------------------------------------------------------------------";
+
+        /// <summary>
+        /// バイトデータのダンプ文字列を返す。
+        /// </summary>
+        /// <returns>バイトデータのダンプ文字列</returns>
+        public override string ToString()
+        {
+            StringBuilder _dumpBuilder = new StringBuilder();
+
+            int _lineCount = 0;
+            bool _firstHeader = true;
+            for (long _offset = 0; _offset < this.ByteData.Length; _offset += 16)
+            {
+                if (this.HeaderFirstLineOnly)
+                {
+                    if (_firstHeader)
+                    {
+                        _dumpBuilder.AppendLine(DUMP_HEADER);
+                        _firstHeader = false;
+                    }
+                }
+                else
+                {
+                    if (this.HeaderSpan > 0 && _lineCount % this.HeaderSpan == 0)
+                    {
+                        _dumpBuilder.AppendLine(DUMP_HEADER);
+                    }
+                }
+
+                byte[] _line = new byte[16];
+                int _lineLength = (int)Math.Min(this.ByteData.Length - _offset, 16);
+                Array.Copy(this.ByteData, _offset, _line, 0, _lineLength);
+
+                string _buffer1 = BitConverter.ToString(_line, 0, (_lineLength < 8) ? _lineLength : 8).Replace('-', ' ');
+                string _buffer2 = (_lineLength < 8) ? "" : BitConverter.ToString(_line, 8, _lineLength - 8).Replace('-', ' ');
+                string _ascii = this.GetAsciiString(_line);
+
+                _dumpBuilder.AppendLine($"{_offset:X8} {_buffer1, -24} {_buffer2, -24}{_ascii}");
+                _lineCount++;
+            }
+
+            return _dumpBuilder.ToString();
+        }
+
+        /// <summary>
+        /// バイトデータのASCII文字列を返す。
+        /// </summary>
+        /// <param name="byteData">バイトデータ</param>
+        /// <returns>バイトデータのASCII文字列</returns>
+        private string GetAsciiString(byte[] byteData)
+        {
+            ASCIIEncoding _encoding = new ASCIIEncoding();
+            StringBuilder _asciiBuilder = new StringBuilder();
+
+            for (int _index = 0; _index < byteData.Length; _index++)
+            {
+                // 制御文字判定
+                if (byteData[_index] >= 0x20 && byteData[_index] <= 0x7E)
+                {
+                    // ASCIIに変換
+                    _asciiBuilder.Append(_encoding.GetString(new byte[] { byteData[_index] }));
+                }
+                else
+                {
+                    // 制御文字はドットに変換
+                    _asciiBuilder.Append('.');
+                }
+            }
+
+            return _asciiBuilder.ToString();
+        }
+    }
+}
---
+++ Byte2Dump/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace Byte2Dump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ByteToDump _byteToDump = new ByteToDump();
+            //_byteToDump.HeaderFirstLineOnly = false;
+            //_byteToDump.HeaderSpan = 10;
+
+            using (FileStream _reader = new FileStream(
+                @"C:\Users\Takashi Kakinuma\Dropbox\Photos\乃木坂46\25th Prof\endousakura_prof.jpg", FileMode.Open))
+            {
+                byte[] _buffer = new byte[1024];
+                long _readByte = _reader.Length;
+
+                while (_readByte > 0)
+                {
+                    _readByte = _reader.Read(_buffer, 0, _buffer.Length);
+                    _byteToDump.Add(_buffer);
+                }
+            }
+
+            //_byteToDump.Add(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 });
+            //_byteToDump.Add(new byte[] { 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F });
+
+            //_byteToDump.Add(new byte[] { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 });
+            //_byteToDump.Add(new byte[] { 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F });
+
+            //_byteToDump.Add(new byte[] { 0x30, 0x31, 0x32, 0x33, 0x41, 0x42, 0x43, 0x44 });
+
+            Console.WriteLine(_byteToDump.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
